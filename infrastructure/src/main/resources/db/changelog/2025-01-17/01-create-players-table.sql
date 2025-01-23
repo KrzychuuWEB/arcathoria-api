625e9e4dde3da7498835b72713a51235a17a238c
Krzysztof <krzychuuweb@gmail.com>
2025-01-23 21:52:32 +0100

    Change architecture to hexagonal from package by features

---
+++ infrastructure/src/main/resources/db/changelog/2025-01-17/01-create-players-table.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset krzysztof:1
+CREATE TABLE players (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE NULL
+);
