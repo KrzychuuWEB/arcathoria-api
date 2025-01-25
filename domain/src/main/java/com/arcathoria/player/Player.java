9a607aaee7c082ea7eeda97d84bc9cdb94187dc4
Krzysztof <krzychuuweb@gmail.com>
2025-01-25 22:08:54 +0100

    Add AssertJ and edit Player domain entity

--- domain/src/main/java/com/arcathoria/player/Player.java
+++ domain/src/main/java/com/arcathoria/player/Player.java
@@ -1,10 +1,54 @@
 package com.arcathoria.player;
 
+import java.time.LocalDateTime;
+
 class Player {
+
+    private Long id;
 
     private final String username;
 
-    Player(final String username) {
+    private final String password;
+
+    private final String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    Player(final String username, final String password, final String email) {
         this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    Long getId() {
+        return id;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    String getEmail() {
+        return email;
+    }
+
+    LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    LocalDateTime getDeletedAt() {
+        return deletedAt;
     }
 }
