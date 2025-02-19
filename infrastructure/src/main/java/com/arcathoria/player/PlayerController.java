11c6bf344c9d4bd136abf936c4bc45c032ab7c01
Krzysztof <krzychuuweb@gmail.com>
2025-02-19 06:48:23 +0100

    Remove Player module

--- infrastructure/src/main/java/com/arcathoria/player/PlayerController.java
+++
@@ -1,9 +0,0 @@
-package com.arcathoria.player;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-class PlayerController {
-
-
-}
--- infrastructure/src/main/java/com/arcathoria/player/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.arcathoria.player;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
-}
