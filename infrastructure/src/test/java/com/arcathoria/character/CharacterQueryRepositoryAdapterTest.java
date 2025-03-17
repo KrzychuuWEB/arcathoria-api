e3bffb5f256a5534a43e28e8ecf99c24af925c00
Krzysztof <krzychuuweb@gmail.com>
2025-03-17 07:24:07 +0100

    Implemented get all characters by account id in repositories

--- infrastructure/src/test/java/com/arcathoria/character/CharacterQueryRepositoryAdapterTest.java
+++ infrastructure/src/test/java/com/arcathoria/character/CharacterQueryRepositoryAdapterTest.java
@@ -4,16 +4,20 @@ import com.arcathoria.PostgreSQLTestContainerConfig;
 import com.arcathoria.account.AccountFacade;
 import com.arcathoria.account.dto.AccountDTO;
 import com.arcathoria.account.dto.RegisterDTO;
+import com.arcathoria.account.vo.AccountId;
 import com.arcathoria.character.vo.CharacterName;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Transactional
 class CharacterQueryRepositoryAdapterTest extends PostgreSQLTestContainerConfig {
 
     @Autowired
@@ -26,13 +30,11 @@ class CharacterQueryRepositoryAdapterTest extends PostgreSQLTestContainerConfig 
     private CharacterQueryRepositoryAdapter queryRepository;
 
     @Test
+    @Transactional
     void should_return_true_if_character_name_exist() {
-        AccountDTO account = accountFacade.createNewAccount(
-                new RegisterDTO("example@email.com", "secretPassword123")
-        );
         Character character = Character.restore(
                 CharacterSnapshotMother.create()
-                        .withAccountId(account.id())
+                        .withAccountId(getAccount().id())
                         .withCharacterName("existCharacterName")
                         .build()
         );
@@ -44,13 +46,11 @@ class CharacterQueryRepositoryAdapterTest extends PostgreSQLTestContainerConfig 
     }
 
     @Test
+    @Transactional
     void should_return_false_if_character_name_not_exist() {
-        AccountDTO account = accountFacade.createNewAccount(
-                new RegisterDTO("example@email.com", "secretPassword123")
-        );
         Character character = Character.restore(
                 CharacterSnapshotMother.create()
-                        .withAccountId(account.id())
+                        .withAccountId(getAccount().id())
                         .withCharacterName("characterName")
                         .build()
         );
@@ -59,5 +59,60 @@ class CharacterQueryRepositoryAdapterTest extends PostgreSQLTestContainerConfig 
         boolean result = queryRepository.existsByName(new CharacterName("character_name"));
 
         assertThat(result).isFalse();
+    }
+
+    @Test
+    void should_return_all_characters_by_account_id() {
+        UUID accountId = getAccount().id();
+        UUID otherAccountId = getAccount().id();
+
+        List<Character> characters = getCharactersList(accountId);
+
+        repository.save(Character.restore(CharacterSnapshotMother.create()
+                .withAccountId(otherAccountId)
+                .withCharacterName("characterName")
+                .build()
+        ));
+
+        List<Character> result = queryRepository.getAllByAccountId(new AccountId(accountId));
+
+        assertThat(result).hasSameSizeAs(characters);
+        assertThat(sortedCharacterAndGetByIndex(result, 0).getCharacterId())
+                .isEqualTo(sortedCharacterAndGetByIndex(characters, 0).getCharacterId());
+        assertThat(sortedCharacterAndGetByIndex(result, 0).getCharacterName())
+                .isEqualTo(sortedCharacterAndGetByIndex(characters, 0).getCharacterName());
+        assertThat(sortedCharacterAndGetByIndex(result, 1).getCharacterId())
+                .isEqualTo(sortedCharacterAndGetByIndex(characters, 1).getCharacterId());
+        assertThat(sortedCharacterAndGetByIndex(result, 1).getCharacterName())
+                .isEqualTo(sortedCharacterAndGetByIndex(characters, 1).getCharacterName());
+    }
+
+    private CharacterSnapshot sortedCharacterAndGetByIndex(List<Character> characters, Integer index) {
+        List<Character> list = characters.stream()
+                .sorted(Comparator.comparing(character -> character.getSnapshot().getCharacterName().value()))
+                .toList();
+
+        return list.get(index).getSnapshot();
+    }
+
+    private List<Character> getCharactersList(UUID accountId) {
+        return List.of(
+                repository.save(Character.restore(CharacterSnapshotMother.create()
+                        .withAccountId(accountId)
+                        .withCharacterName("characterName")
+                        .build()
+                )),
+                repository.save(Character.restore(CharacterSnapshotMother.create()
+                        .withAccountId(accountId)
+                        .withCharacterName("characterName")
+                        .build()
+                ))
+        );
+    }
+
+    private AccountDTO getAccount() {
+        return accountFacade.createNewAccount(
+                new RegisterDTO("test_" + UUID.randomUUID() + "@email.com", "secretPassword123")
+        );
     }
 }
