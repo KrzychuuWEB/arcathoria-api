6acd935927a1943ecaeacae20812a576b09da78f
Krzysztof <krzychuuweb@gmail.com>
2025-03-30 13:24:53 +0200

    feat (character): refactored IntegrationTestContainersConfig

--- infrastructure/src/test/java/com/arcathoria/IntegrationTestContainersConfig.java
+++ infrastructure/src/test/java/com/arcathoria/IntegrationTestContainersConfig.java
@@ -1,37 +1,44 @@
 package com.arcathoria;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+@TestConfiguration
 @Testcontainers
 @ActiveProfiles("test")
 public abstract class IntegrationTestContainersConfig {
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgresContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("test")
-                    .withUsername("test")
-                    .withPassword("test");
+    private static final int REDIS_PORT = 6379;
+    private static final String REDIS_PASSWORD = "test_secret_pass";
 
-    @Container
-    static GenericContainer<?> redisContainer =
-            new GenericContainer<>(DockerImageName.parse("redis:7"))
-                    .withExposedPorts(6379)
-                    .withCommand("redis-server", "--requirepass", "test_secret_pass");
+    @ServiceConnection
+    static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(true);
+
+    static final GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:7"))
+            .withExposedPorts(REDIS_PORT)
+            .withCommand("redis-server", "--requirepass", REDIS_PASSWORD)
+            .withReuse(true);
+
+    static {
+        postgresContainer.start();
+        redisContainer.start();
+    }
 
     @DynamicPropertySource
     static void registerRedisProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.redis.host", redisContainer::getHost);
-        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379).toString());
-        registry.add("spring.data.redis.password", () -> "test_secret_pass");
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(REDIS_PORT));
+        registry.add("spring.data.redis.password", () -> REDIS_PASSWORD);
     }
 }
