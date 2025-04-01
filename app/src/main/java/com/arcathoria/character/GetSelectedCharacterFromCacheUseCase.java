ea7ba796bea16525d922d538c13adc1bd57004d8
Krzysztof <krzychuuweb@gmail.com>
2025-04-01 07:22:55 +0200

    feat (character): added get selected character to application layer and added method to CharacterQueryFacade

---
+++ app/src/main/java/com/arcathoria/character/GetSelectedCharacterFromCacheUseCase.java
@@ -0,0 +1,23 @@
+package com.arcathoria.character;
+
+import com.arcathoria.account.vo.AccountId;
+import com.arcathoria.character.vo.CharacterId;
+
+class GetSelectedCharacterFromCacheUseCase {
+
+    private final GetCharacterByIdUseCase getCharacterByIdUseCase;
+    private final SelectCharacterCachePort selectCharacterCachePort;
+
+    GetSelectedCharacterFromCacheUseCase(final GetCharacterByIdUseCase getCharacterByIdUseCase,
+                                         final SelectCharacterCachePort selectCharacterCachePort
+    ) {
+        this.getCharacterByIdUseCase = getCharacterByIdUseCase;
+        this.selectCharacterCachePort = selectCharacterCachePort;
+    }
+
+    Character execute(final AccountId accountId) {
+        CharacterId characterId = selectCharacterCachePort.getAndSetNewExpiredTime(accountId);
+
+        return getCharacterByIdUseCase.getOwned(characterId, accountId);
+    }
+}
