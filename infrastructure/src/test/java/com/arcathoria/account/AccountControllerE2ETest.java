775e20e8e8fffe64946fb75a201cf70e7dfb3f6f
Krzysztof <krzychuuweb@gmail.com>
2025-04-10 20:23:32 +0200

    refactor(character, account): changed integration test to e2e test for controllers. Created helpers for e2e tests

--- infrastructure/src/test/java/com/arcathoria/account/AccountControllerE2ETest.java
+++ infrastructure/src/test/java/com/arcathoria/account/AccountControllerE2ETest.java
@@ -4,8 +4,6 @@ import com.arcathoria.ApiErrorResponse;
 import com.arcathoria.IntegrationTestContainersConfig;
 import com.arcathoria.account.dto.AccountDTO;
 import com.arcathoria.account.dto.RegisterDTO;
-import com.arcathoria.account.vo.Email;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,27 +11,22 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AccountControllerIT extends IntegrationTestContainersConfig {
-
-    @Autowired
-    private AccountFacade accountFacade;
+class AccountControllerE2ETest extends IntegrationTestContainersConfig {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private final String BASE_URL = "/accounts";
+    private final String registerUrl = "/accounts/register";
 
     @Test
-    @Transactional
     void should_register_account_and_return_201_status() {
-        RegisterDTO registerDTO = new RegisterDTO("account@email.com", "secret_password");
+        RegisterDTO registerDTO = RegisterDTOMother.aRegisterDTO().withEmail("accountRegister@email.com").build();
 
-        ResponseEntity<AccountDTO> response = restTemplate.postForEntity(BASE_URL + "/register", new HttpEntity<>(registerDTO), AccountDTO.class);
+        ResponseEntity<AccountDTO> response = restTemplate.postForEntity(registerUrl, new HttpEntity<>(registerDTO), AccountDTO.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(response.getBody()).isNotNull();
@@ -41,14 +34,12 @@ class AccountControllerIT extends IntegrationTestContainersConfig {
     }
 
     @Test
-    @Sql(statements = "TRUNCATE TABLE accounts RESTART IDENTITY", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void should_return_409_when_email_already_exists() {
-        Email email = new Email("account@email.com");
-        RegisterDTO registerDTO = new RegisterDTO(email.value(), "secret_password");
+        RegisterDTO registerDTO = RegisterDTOMother.aRegisterDTO().withEmail("takenRegisterAccount@email.com").build();
 
-        accountFacade.createNewAccount(registerDTO);
+        restTemplate.postForEntity(registerUrl, new HttpEntity<>(registerDTO), AccountDTO.class);
 
-        ResponseEntity<ApiErrorResponse> result = restTemplate.postForEntity(BASE_URL + "/register", new HttpEntity<>(registerDTO), ApiErrorResponse.class);
+        ResponseEntity<ApiErrorResponse> result = restTemplate.postForEntity(registerUrl, new HttpEntity<>(registerDTO), ApiErrorResponse.class);
 
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
     }
