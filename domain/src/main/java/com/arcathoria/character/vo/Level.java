d90d195bb07505182d3ff413ca7021b997694897
Krzysztof <krzychuuweb@gmail.com>
2025-09-05 17:26:45 +0200

    feat(character): introduce Level value object with validation and unit tests
    - Added `Level` as a value object for character representation with validation to ensure positive levels.
    - Implemented methods for incrementing level and retrieving the next level.
    - Included comprehensive unit tests to validate functionality and constraints.

---
+++ domain/src/main/java/com/arcathoria/character/vo/Level.java
@@ -0,0 +1,20 @@
+package com.arcathoria.character.vo;
+
+public record Level(int level) {
+
+    public Level {
+        if (level < 1) {
+            throw new IllegalArgumentException("Level must be greater than 0");
+        }
+    }
+
+    public Level incrementBy(int delta) {
+        if (delta < 0) throw new IllegalArgumentException("Level delta must be greater than 0");
+        int raw = Math.addExact(this.level, delta);
+        return new Level(raw);
+    }
+
+    public Level next() {
+        return incrementBy(1);
+    }
+}
---
+++ domain/src/test/java/com/arcathoria/character/vo/LevelTest.java
@@ -0,0 +1,45 @@
+package com.arcathoria.character.vo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LevelTest {
+
+    @Test
+    void should_return_valid_level() {
+        int exampleLevel = 10;
+
+        Level level = new Level(exampleLevel);
+
+        assertThat(level.level()).isEqualTo(exampleLevel);
+    }
+
+    @Test
+    void should_return_new_level_after_increment() {
+        Level level = new Level(1);
+
+        Level newLevel = level.incrementBy(1);
+
+        assertThat(newLevel.level()).isEqualTo(2);
+    }
+
+    @Test
+    void should_return_exception_when_level_is_below_zero() {
+        Level level = new Level(1);
+
+        assertThatThrownBy(() -> level.incrementBy(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .message().isEqualTo("Level delta must be greater than 0");
+    }
+
+    @Test
+    void should_return_next_level_with_one_level_increment() {
+        Level level = new Level(1);
+
+        Level nextLevel = level.next();
+
+        assertThat(nextLevel.level()).isEqualTo(2);
+    }
+}
