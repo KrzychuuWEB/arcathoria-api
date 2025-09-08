76b405ef2dece582d05ad2450ab22245526c9db9
Krzysztof <krzychuuweb@gmail.com>
2025-09-08 15:23:51 +0200

    feat(combat): add handling for wrong turn exception
    - Introduced `WrongTurnException` to handle cases where an invalid combat turn action is performed.
    - Added internationalized error messages for both English and Polish.
    - Updated `CombatExceptionHandler` to manage the new exception with appropriate HTTP conflict response.

--- infrastructure/src/main/java/com/arcathoria/combat/CombatExceptionHandler.java
+++ infrastructure/src/main/java/com/arcathoria/combat/CombatExceptionHandler.java
@@ -2,6 +2,7 @@ package com.arcathoria.combat;
 
 import com.arcathoria.ApiErrorResponse;
 import com.arcathoria.combat.exception.CombatParticipantUnavailableException;
+import com.arcathoria.combat.exception.WrongTurnException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -34,6 +35,20 @@ class CombatExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 messageSource.getMessage("combat.initial.participant.not.found", new Object[]{ex.getCombatSide()}, ex.getMessage(), locale),
+                ex.getErrorCode(),
+                request.getRequestURI()
+        );
+    }
+
+    @ExceptionHandler(WrongTurnException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    ApiErrorResponse handleWrongTurnException(final WrongTurnException ex, final HttpServletRequest request, final Locale locale) {
+        logger.warn("Currently the turn belongs to {}", ex.getCombatSide());
+
+        return new ApiErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                messageSource.getMessage("combat.combat.turn.conflict", new Object[]{ex.getCombatSide()}, ex.getMessage(), locale),
                 ex.getErrorCode(),
                 request.getRequestURI()
         );
