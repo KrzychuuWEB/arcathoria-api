1db8da0caa7b634ad77bb40f17fb07c599f0ffff
Krzysztof <krzychuuweb@gmail.com>
2025-09-10 14:19:01 +0200

    refactor(combat): added new test for MeleeCombatActionStrategyTest

--- domain/src/test/java/com/arcathoria/combat/MeleeCombatActionStrategyTest.java
+++ domain/src/test/java/com/arcathoria/combat/MeleeCombatActionStrategyTest.java
@@ -1,11 +1,16 @@
 package com.arcathoria.combat;
 
 import com.arcathoria.character.vo.Level;
+import com.arcathoria.combat.exception.WrongTurnException;
 import com.arcathoria.combat.vo.CombatTurn;
+import com.arcathoria.combat.vo.ParticipantId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MeleeCombatActionStrategyTest {
 
@@ -34,5 +39,26 @@ class MeleeCombatActionStrategyTest {
         meleeCombatActionStrategy.execute(combat, Participant.restore(attacker));
 
         assertThat(combat.getSnapshot().defender().health().getCurrent()).isEqualTo(92);
+    }
+
+    @Test
+    void should_throw_WrongTurnException_when_defender_acts_on_attackers_turn() {
+        ParticipantSnapshot attacker = ParticipantSnapshotMother.aParticipantBuilder()
+                .withId(new ParticipantId(UUID.randomUUID()))
+                .withIntelligence(new Level(1))
+                .withHealth(150, 150)
+                .build();
+        ParticipantSnapshot defender = ParticipantSnapshotMother.aParticipantBuilder()
+                .withId(new ParticipantId(UUID.randomUUID()))
+                .withHealth(100, 100)
+                .build();
+        Combat combat = Combat.restore(CombatSnapshotMother.aCombat()
+                .withAttacker(attacker)
+                .withDefender(defender)
+                .withCombatTurn(new CombatTurn(CombatSide.ATTACKER))
+                .build());
+
+        assertThatThrownBy(() -> meleeCombatActionStrategy.execute(combat, Participant.restore(defender)))
+                .isInstanceOf(WrongTurnException.class);
     }
 }
